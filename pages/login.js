5d046d66f60243e0319cbd3bcc090ef320dc8020
park-jemin <jemin@live.unc.edu>
2022-03-09 10:18:07 -0500

    TODO: login, signup, middleware

---
+++ pages/login.js
@@ -0,0 +1,20 @@
+/*
+  TODO: Login View
+
+  Use OAuth to allow users to login using email/password
+  Also offers Next/Link to Sign-up and possibly forgot password procedures
+ */
+
+const Login = () => {
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-black">
+      {/* SampleVu Logo */}
+      {/* Login form */}
+      {/* Login button */}
+      {/* Sign-up link */}
+      {/* Forgot password link */}
+    </div>
+  )
+}
+
+export default Login
---
+++ pages/signup.js
@@ -0,0 +1,19 @@
+/*
+  TODO: Signup View
+
+  Use OAuth to allow users to create accounts using email/password
+  Also offers Next/Link to Login
+ */
+
+const Signup = () => {
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-black">
+      {/* SampleVu Logo */}
+      {/* Sign-up form */}
+      {/* Sign-up button */}
+      {/* Login link */}
+    </div>
+  )
+}
+
+export default Signup
