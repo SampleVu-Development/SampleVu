b7a062c43ceb711d6d167c34f6b16841b063daff
park-jemin <jemin@live.unc.edu>
2022-03-09 10:07:51 -0500

    TODO: invites redirect

---
+++ pages/invites/[invite_id].js
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router'
+
+/*
+ * TODO: Invited View
+ * Refer to dynamic routing documentation here:
+ * https://nextjs.org/docs/routing/dynamic-routes
+ *
+ * Sends a request through invite API route to add user to project if user exists,
+ * otherwise directs user to sign-up/login to create account
+ *
+ *
+ * Also may want to use server-side rendering or generation (SSR/SSG),
+ * so use getServerSideProps or getStaticProps
+ */
