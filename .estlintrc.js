a533f1e7e028ad99dcca64e91f4bb284dfa0b0a0
park-jemin <jemin@live.unc.edu>
2022-03-09 02:11:27 -0500

    eslint & prettier config

---
+++ .estlintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
+  plugins: ['react', 'prettier'],
+}
--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   singleQuote: true,
   semi: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  useTabs: false,
+  bracketSpacing: true,
+  printWidth: 100,
 }
