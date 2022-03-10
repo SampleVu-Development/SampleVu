a89377d204605bd4cbdf02063cd434cd7b3ea261
park-jemin <jemin@live.unc.edu>
2022-03-10 18:19:30 -0500

    Added Survey Button TODOs

--- components/survey/buttons/SaveSurveyButton.js
+++ components/survey/buttons/SaveSurveyButton.js
@@ -1,4 +1,5 @@
 /*
   TODO: Add save button, where upon press, saves survey to database via API
+  Validates no questions are currently being created
   API to be added later
  */
---
+++ components/survey/buttons/SubmitSurveyButton.js
@@ -0,0 +1,5 @@
+/*
+  TODO: Add submit button, where upon press, submits filled survey form to database via API
+  Validates all (required) questions have answers before submitting
+  API to be added later
+ */
