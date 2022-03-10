e157fc8ebe4c2ced04a4f80795c4818bac12acc0
park-jemin <jemin@live.unc.edu>
2022-03-10 18:09:45 -0500

    Update CreateQuestion w/ Tailwind

--- components/survey/CreateQuestion.js
+++ components/survey/CreateQuestion.js
@@ -1,102 +1,122 @@
-import { Card, Button, Modal, Input, Radio } from 'antd';
-import { PlusCircleOutlined, DeleteOutlined } from '@ant-design/icons';
-import styles from './CreateQuestion.module.css';
-import { useState } from 'react';
-const { TextArea } = Input;
+import { Card, Button, Modal, Input, Radio } from 'antd'
+import { PlusCircleOutlined, DeleteOutlined } from '@ant-design/icons'
+import { useState } from 'react'
+const { TextArea } = Input
 
-export default function CreateQuestion({callback}) {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const defaultText = "type option here..."
-  const [questionTitle, setQuestionTitle] = useState("")
+export default function CreateQuestion({ callback }) {
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const defaultText = 'type option here...'
+  const [questionTitle, setQuestionTitle] = useState('')
   const [currentOptions, setOptions] = useState([defaultText])
 
   const addQuestion = () => {
     currentOptions.push(defaultText)
-    setOptions([...currentOptions] )
+    setOptions([...currentOptions])
   }
 
-  const deleteQuestion = (i) => {
+  const deleteQuestion = i => {
     currentOptions.splice(i, 1)
     const newOptions = [...currentOptions]
     setOptions(newOptions)
     const optionDisplay = document.getElementById('optionDisplay')
-    initOptionDisplay(newOptions);
-    optionDisplay = currentOptionDisplay;
+    initOptionDisplay(newOptions)
+    optionDisplay = currentOptionDisplay
   }
 
-
   const updateQuestion = (i, e) => {
-    currentOptions[i] = e.target.value;
+    currentOptions[i] = e.target.value
     setOptions([...currentOptions])
   }
 
   let currentOptionDisplay = []
-  
-  const initOptionDisplay = (options) => {
+
+  const initOptionDisplay = options => {
     currentOptionDisplay = []
     for (let i = 0; i < options.length; i++) {
       if (options[i] == defaultText) {
         currentOptionDisplay.push(
           <div>
-            <Radio className={styles.choiceDiv}>
-              <Input bordered={false} placeholder={options[i]} onChange={(e) => updateQuestion(i, e)}/>
+            <Radio className="w-4/5">
+              <Input
+                bordered={false}
+                placeholder={options[i]}
+                onChange={e => updateQuestion(i, e)}
+              />
             </Radio>
-            <Button className="no-border curvedBorder" onClick={() => deleteQuestion(i)}><DeleteOutlined /></Button>
+            <Button className="no-border curvedBorder" onClick={() => deleteQuestion(i)}>
+              <DeleteOutlined />
+            </Button>
           </div>
         )
       } else {
         currentOptionDisplay.push(
           <div>
-            <Radio className={styles.choiceDiv}>
-              <Input bordered={false} defaultValue={options[i]} onChange={(e) => updateQuestion(i, e)}/>
+            <Radio className="w-4/5">
+              <Input
+                bordered={false}
+                defaultValue={options[i]}
+                onChange={e => updateQuestion(i, e)}
+              />
             </Radio>
-            <Button className="no-border curvedBorder" onClick={() => deleteQuestion(i)}><DeleteOutlined /></Button>
+            <Button className="no-border curvedBorder" onClick={() => deleteQuestion(i)}>
+              <DeleteOutlined />
+            </Button>
           </div>
         )
       }
     }
   }
 
-  initOptionDisplay(currentOptions);
+  initOptionDisplay(currentOptions)
 
   const showModal = () => {
     setOptions([defaultText])
-    setQuestionTitle("")
-    setIsModalVisible(true);
-  };
+    setQuestionTitle('')
+    setIsModalVisible(true)
+  }
   const handleOk = () => {
     // SET VARIABLES TO STATE AND ADD A NEW QUESTION TO THE VIEW
-    callback({type: 'create', title: questionTitle, data: [...currentOptions]})
-    setIsModalVisible(false);
-  };
+    callback({ type: 'create', title: questionTitle, data: [...currentOptions] })
+    setIsModalVisible(false)
+  }
 
   const handleCancel = () => {
     // RESET ALL VARIABLES TO INITIAL STATES IN MODAL
     setIsModalVisible(false)
-  };
+  }
 
   const afterClose = () => {
     const inputModal = document.getElementById('inputModal')
   }
 
   const modal = (
-      <Modal title="Create Question" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} afterClose={afterClose} destroyOnClose={true}>
-          <Card className="no-border">
-            <TextArea rows={1} id="inputModal" className="curvedBorder" 
-              placeholder="Write question here..." 
-              onChange={(e) => setQuestionTitle(e.target.value)}
-            />
-            <br />
-            <br />
-            <div id="optionDisplay">
-              {currentOptionDisplay}
-            </div>
-            <Card bordered={false}>
-              <Button className='curvedBorder' onClick={addQuestion}><PlusCircleOutlined /> Add option</Button>
-            </Card>
-          </Card>
-        </Modal>
-    )
+    <Modal
+      title="Create Question"
+      visible={isModalVisible}
+      onOk={handleOk}
+      onCancel={handleCancel}
+      afterClose={afterClose}
+      destroyOnClose={true}
+    >
+      <Card className="no-border">
+        <TextArea
+          rows={1}
+          id="inputModal"
+          className="curvedBorder"
+          placeholder="Write question here..."
+          onChange={e => setQuestionTitle(e.target.value)}
+        />
+        <br />
+        <br />
+        <div id="optionDisplay">{currentOptionDisplay}</div>
+        <Card bordered={false}>
+          <Button className="curvedBorder" onClick={addQuestion}>
+            <PlusCircleOutlined /> Add option
+          </Button>
+        </Card>
+      </Card>
+    </Modal>
+  )
 
   return (
     <>
@@ -107,7 +127,6 @@ export default function CreateQuestion({callback}) {
         </Button>
       </div>
       {modal}
-      
     </>
   )
 }
--- components/survey/ReOrderBlock.js
+++ components/survey/ReOrderBlock.js
@@ -1,29 +1,30 @@
+import { UpOutlined, DownOutlined } from '@ant-design/icons'
+import { Button } from 'antd'
+
 import styles from './ReOrderBlock.module.css'
-import { UpOutlined, DownOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
 
 function moveUp() {
   // update state here
-  console.log('move up');
+  console.log('move up')
 }
 
 function moveDown() {
   // update state here
-  console.log('move down');
+  console.log('move down')
 }
 
-export default function ReOrderBlock () {
+export default function ReOrderBlock() {
   return (
     <div className={styles.reOrderContainer}>
-        <div className={styles.reOrderTitle}> Re-order question</div> 
-        <div className={styles.reOrderBtn}> 
-          <Button type="text">
-            <UpOutlined onClick={moveUp} /> 
-          </Button>
-          <Button type="text">
-            <DownOutlined onClick={moveDown}/> 
-          </Button>
-        </div>
+      <div className={styles.reOrderTitle}> Re-order question</div>
+      <div className={styles.reOrderBtn}>
+        <Button type="text">
+          <UpOutlined onClick={moveUp} />
+        </Button>
+        <Button type="text">
+          <DownOutlined onClick={moveDown} />
+        </Button>
+      </div>
     </div>
   )
 }
