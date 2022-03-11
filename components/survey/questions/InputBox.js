0459fed54e62777000776a6496ca308c07afd3b4
park-jemin <jemin@live.unc.edu>
2022-03-11 17:43:14 -0500

    Update InputBox.js

--- components/survey/questions/InputBox.js
+++ components/survey/questions/InputBox.js
@@ -1,10 +1,15 @@
-import { Input, Card } from 'antd';
-const { TextArea } = Input;
+import { Input, Card } from 'antd'
+const { TextArea } = Input
 
-export default function InputBox ({questionID, title, callback}) {
+export default function InputBox({ questionID, title, callback }) {
   return (
     <Card bordered={false}>
-      <TextArea rows={1} className="curvedBorder" placeholder="Write a comment..." onChange={(e) => callback({title, questionID, type: "input", event: e.target.value})}/>
+      <TextArea
+        rows={1}
+        className="rounded-xl border-gray-300"
+        placeholder="Write a comment..."
+        onChange={e => callback({ title, questionID, type: 'input', event: e.target.value })}
+      />
     </Card>
   )
 }
