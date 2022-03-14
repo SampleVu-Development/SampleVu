9aff0a0e28282e61a7155eec0053dd57c8132951
szhu00 <szhu0520@live.unc.edu>
2022-03-13 22:16:07 -0400

    rm-ed face icons from star rating

--- components/survey/questions/StarRating.js
+++ components/survey/questions/StarRating.js
@@ -1,18 +1,13 @@
 import { Rate, Card } from 'antd'
-import { FrownOutlined, SmileOutlined } from '@ant-design/icons'
 
 export default function StarRating({ callback }) {
   return (
     <div className="flex" bordered={false}>
-      <div className="flex flex-1 p-1">
-        <FrownOutlined />
-      </div>
+      <div className="flex flex-1 p-1"></div>
       <div className="w-3/4">
         <Rate onChange={callback} />
       </div>
-      <div className="flex flex-1 p-1">
-        <SmileOutlined />
-      </div>
+      <div className="flex flex-1 p-1"></div>
     </div>
   )
 }
