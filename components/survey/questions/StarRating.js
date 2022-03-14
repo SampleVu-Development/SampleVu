a503b563958e61e27cf9c7fb13e24d6877f1a883
David Lu <davidlu51412@gmail.com>
2022-03-13 21:20:11 -0400

    remove module starrating

--- components/survey/questions/StarRating.js
+++ components/survey/questions/StarRating.js
@@ -1,6 +1,5 @@
 import { Rate, Card } from 'antd'
 import { FrownOutlined, SmileOutlined } from '@ant-design/icons'
-import styles from './SurveySlider.module.css'
 
 export default function StarRating({ callback }) {
   return (
@@ -9,7 +8,7 @@ export default function StarRating({ callback }) {
         <FrownOutlined />
       </div>
       <div className="w-3/4">
-        <Rate className={styles.surveySliderItem} onChange={callback} />
+        <Rate onChange={callback} />
       </div>
       <div className="flex flex-1 p-1">
         <SmileOutlined />
