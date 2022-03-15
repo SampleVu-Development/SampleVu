74a8080e8d2463ff8dd1a033bee3b602420687e2
David Lu <davidlu51412@gmail.com>
2022-03-15 14:20:30 -0500

    very simple  drag and drop reordering- needs to look better and actually log new order

--- pages/survey-demo.js
+++ pages/survey-demo.js
@@ -2,27 +2,34 @@ import Head from 'next/head'
 import SurveyCard from '../components/survey/SurveyCard'
 import OverallRatingCard from '../components/survey/OverallRatingCard'
 import PageLayout from '../components/PageLayout'
-
-function surveyQuestionCallback(information) {
-  console.log('question information:')
-  for (const key in information) {
-    console.log(key + ': ' + information[key])
-  }
-}
-
-let multiChoices = [
-  'Not sweet enough',
-  'Not quite sweet enough',
-  'Perfect',
-  'A little too sweet',
-  'Much too sweet',
-]
-let multiChoices2 = ['Too Dark', 'Perfect', 'Too Light']
-let question0ID = 0
-let question1ID = 1
-let question2ID = 2
+import Sortable from 'sortablejs'
+import { useEffect } from 'react'
 
 export default function Survey() {
+  function surveyQuestionCallback(information) {
+    console.log('question information:')
+    for (const key in information) {
+      console.log(key + ': ' + information[key])
+    }
+  }
+
+  let multiChoices = [
+    'Not sweet enough',
+    'Not quite sweet enough',
+    'Perfect',
+    'A little too sweet',
+    'Much too sweet',
+  ]
+  let multiChoices2 = ['Too Dark', 'Perfect', 'Too Light']
+  let question0ID = 0
+  let question1ID = 1
+  let question2ID = 2
+
+  useEffect(() => {
+    var el = document.getElementById('questions')
+    var sortable = Sortable.create(el)
+  }, [])
+
   return (
     <>
       <Head>
@@ -42,21 +49,22 @@ export default function Survey() {
 
           {/* Entire area should be "Droppable" */}
           {/* Questions array .map to "Draggable" SurveyCards */}
-
-          <SurveyCard
-            type="multiple"
-            question="How does this sample meet your needs for sweetness?"
-            data={multiChoices}
-            callback={surveyQuestionCallback}
-            questionID={question1ID}
-          />
-          <SurveyCard
-            type="multiple"
-            question="How would you rate the color of this sample?"
-            data={multiChoices2}
-            callback={surveyQuestionCallback}
-            questionID={question2ID}
-          />
+          <div id="questions">
+            <SurveyCard
+              type="multiple"
+              question="How does this sample meet your needs for sweetness?"
+              data={multiChoices}
+              callback={surveyQuestionCallback}
+              questionID={question1ID}
+            />
+            <SurveyCard
+              type="multiple"
+              question="How would you rate the color of this sample?"
+              data={multiChoices2}
+              callback={surveyQuestionCallback}
+              questionID={question2ID}
+            />
+          </div>
 
           <SurveyCard type="create" callback={surveyQuestionCallback} />
 
