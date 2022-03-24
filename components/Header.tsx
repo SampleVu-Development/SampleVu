5c234d714976d6ce6ba2da7196309bf7184dd73f
szhu00 <szhu0520@live.unc.edu>
2022-03-24 15:08:48 -0400

    Drawer in progress

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Layout, Avatar, Dropdown, Menu } from 'antd'
+import { Layout, Avatar, Dropdown } from 'antd'
 import { MenuOutlined, DownOutlined } from '@ant-design/icons'
 import Image from 'next/image'
 import logo from '../public/logo/V_Logo.png'
+import DropDownMenu from './DropDownMenu'
 
 // attempt to use @svgr/webpack
 import Logo from '../public/logo/SampleVu_Logo.svg'
-import DropDownMenu from './DropDownMenu'
 
 /*
   TODO: Header
@@ -18,13 +18,13 @@ import DropDownMenu from './DropDownMenu'
 const { Header } = Layout
 const TopHeader = ({ pageName }) => {
   const header = (
-    <Header className="px-15 sticky top-0 z-30 w-full">
+    <Header className="px-18 sticky top-0 z-30 w-full">
       <div className="text-slate-700 flex items-center justify-between">
         <div className=" flex items-center">
           <span>
             <MenuOutlined className="text-2xl text-white" />
           </span>
-          <a href="/" className=" flex p-4">
+          <a href="/" className=" flex pl-8">
             <Image src={logo} alt="logo" width={30} height={30} className="hover:bg-bannerHover" />
             <h1 className="pl-1 text-2xl font-bold text-white">SampleVu</h1>
           </a>
@@ -32,17 +32,15 @@ const TopHeader = ({ pageName }) => {
 
         {/* this link should take user to user profile but for right now I'll just set it to "/"  */}
         <div className="flex items-center">
-          <Dropdown overlay={DropDownMenu}>
+          <Dropdown overlay={DropDownMenu} className="mr-10" placement="bottom">
             <div className="flex items-center">
               <Avatar className=" float-right bg-purple" size="large">
                 {/* pass in user later  */}
                 FL
               </Avatar>
-              <DownOutlined className="ml-2 text-2xl font-bold text-white" />
+              <DownOutlined className="ml-2  text-white" style={{ fontSize: '14px' }} />
             </div>
           </Dropdown>
-
-          {/* <h1 className=" ml-3 text-white"> firstName L </h1> */}
         </div>
       </div>
     </Header>
