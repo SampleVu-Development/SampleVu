10376d38307d57c7aa89188855abaa4d93429e85
joshk5371 <joshk5371@gmail.com>
2022-04-08 17:57:05 -0400

    Added question dropdown on results page

--- pages/results.tsx
+++ pages/results.tsx
@@ -10,8 +10,19 @@ import {
   Row,
   Col,
   Divider,
+  Select,
 } from 'antd'
 import PageLayout from '../components/PageLayout'
+
+const { Option } = Select
+
+function onChange(value) {
+  console.log(`selected ${value}`)
+}
+
+function onSearch(val) {
+  console.log('search:', val)
+}
 
 export default function Results() {
   return (
@@ -33,6 +44,23 @@ export default function Results() {
               <Card>
                 <PageHeader className="p-0" title="Results by Question"></PageHeader>
                 <Divider />
+                <Select
+                  showSearch
+                  style={{ width: '95%' }}
+                  placeholder="Select a question"
+                  optionFilterProp="children"
+                  onChange={onChange}
+                  onSearch={onSearch}
+                  filterOption={(input, option) =>
+                    option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                  }
+                >
+                  <Option value="q1">Question 1: Rate the SWEETNESS of this sample</Option>
+                  <Option value="q2">Question 2: Rate the ACIDITY of this sample</Option>
+                  <Option value="q3">Question 3: Rate the COLOR of this sample</Option>
+                  <Option value="q4">Question 4: Rate the AROMA of this sample</Option>
+                  <Option value="q5">Question 5: How would you rate this sample OVERALL?</Option>
+                </Select>
               </Card>
             </Col>
           </Row>
