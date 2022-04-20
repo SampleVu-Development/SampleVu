ba2111a15961aedaa63f2a3362557357fe4068fe
szhu00 <szhu0520@live.unc.edu>
2022-04-20 00:20:23 -0400

    project dashboard link

---
+++ pages/project-dashboard.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import PageLayout from '../components/PageLayout'
+
+export default function ProjectDashboard() {
+  return (
+    <>
+      <head>
+        <title>SampleVu</title>
+        <meta name="SampleVu helps companies organize and supply" content="SampleVu" />
+      </head>
+      <PageLayout pageName="Projects">
+        <div>hi</div>
+      </PageLayout>
+    </>
+  )
+}
