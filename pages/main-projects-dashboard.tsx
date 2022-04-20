fddd447e902b92280a00ec20b997e0a6e8135f66
szhu00 <szhu0520@live.unc.edu>
2022-04-20 12:57:40 -0400

    dashboard patch

--- pages/main-projects-dashboard.tsx
+++ pages/main-projects-dashboard.tsx
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 import PageLayout from '../components/PageLayout'
 
-export default function ProjectDashboard() {
+export default function MainProjectsDashboard() {
   return (
     <>
       <head>
         <title>SampleVu</title>
         <meta name="SampleVu helps companies organize and supply" content="SampleVu" />
       </head>
-      <PageLayout pageName="Winter Frost">
+      <PageLayout pageName="Projects">
         <div>hi</div>
       </PageLayout>
     </>
