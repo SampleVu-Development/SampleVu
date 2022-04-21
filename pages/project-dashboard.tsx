a8a6a7ed2bbb313d56a9dd4c01e5806a6f20e46f
szhu00 <szhu0520@live.unc.edu>
2022-04-21 16:08:52 -0400

    adding cards

--- pages/project-dashboard.tsx
+++ pages/project-dashboard.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import PageLayout from '../components/PageLayout'
+import { Row, Col, Card, PageHeader, Divider } from 'antd'
 
 export default function ProjectDashboard() {
   return (
@@ -9,7 +10,16 @@ export default function ProjectDashboard() {
         <meta name="SampleVu helps companies organize and supply" content="SampleVu" />
       </head>
       <PageLayout pageName="Winter Frost">
-        <div>hi</div>
+        <div className="m-2.5 max-h-full">
+          <Row gutter={[16, 0]}>
+            <Col flex="auto">
+              <Card title="Project Details" className="h-full"></Card>
+            </Col>
+            <Col flex="auto">
+              <Card title="Survey"></Card>
+            </Col>
+          </Row>
+        </div>
       </PageLayout>
     </>
   )
