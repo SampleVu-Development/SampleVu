87653445e314549028fcea2f26896395a351c86d
joshk5371 <joshk5371@gmail.com>
2022-04-22 03:21:41 -0400

    Added charts for each sample and provided functionality to checkboxes

--- pages/results.tsx
+++ pages/results.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
-import { useState } from 'react'
-import { Card, PageHeader, Table, Row, Col, Divider, Select, Rate } from 'antd'
+import { useState, useEffect } from 'react'
+import * as ReactDOM from 'react-dom'
+import { Card, PageHeader, Table, Row, Col, Divider, Select, Rate, Space } from 'antd'
+import { Column } from '@ant-design/plots'
 import PageLayout from '../components/PageLayout'
 
 const { Option } = Select
@@ -18,7 +20,7 @@ const columns = [
   },
   {
     title: 'Responses',
-    dataIndex: 'responses',
+    dataIndex: 'numberOfResponses',
   },
 ]
 
@@ -26,39 +28,150 @@ interface DataType {
   key: React.Key
   sample: string
   rating: number
-  responses: number
+  numberOfResponses: number
 }
 
-const data: DataType[] = [
+const sampleInfo: DataType[] = [
   {
-    key: '1',
+    key: '0',
     sample: '59468-622',
     rating: 2.5,
-    responses: 32,
+    numberOfResponses: 32,
+  },
+  {
+    key: '1',
+    sample: '23155-438',
+    rating: 1,
+    numberOfResponses: 32,
   },
   {
     key: '2',
-    sample: '23155-438',
-    rating: 1,
-    responses: 32,
+    sample: '145652-435',
+    rating: 5,
+    numberOfResponses: 32,
   },
   {
     key: '3',
-    sample: '145652-435',
-    rating: 5,
-    responses: 32,
-  },
-  {
-    key: '4',
     sample: '866392-926',
     rating: 4,
-    responses: 32,
+    numberOfResponses: 32,
   },
 ]
 
+const results = [
+  {
+    key: 0,
+    sample: '59468-622',
+    question: 'q1',
+    numberOfResponses: [
+      { sample: '59468-622', type: '1', responses: 2 },
+      { sample: '59468-622', type: '2', responses: 3 },
+      { sample: '59468-622', type: '3', responses: 7 },
+      { sample: '59468-622', type: '4', responses: 10 },
+      { sample: '59468-622', type: '5', responses: 10 },
+    ],
+  },
+
+  {
+    key: 1,
+    sample: '23155-438',
+    question: 'q1',
+    numberOfResponses: [
+      { sample: '23155-438', type: '1', responses: 10 },
+      { sample: '23155-438', type: '2', responses: 5 },
+      { sample: '23155-438', type: '3', responses: 12 },
+      { sample: '23155-438', type: '4', responses: 5 },
+      { sample: '23155-438', type: '5', responses: 0 },
+    ],
+  },
+  {
+    key: 2,
+    sample: '145652-435',
+    question: 'q1',
+    numberOfResponses: [
+      { sample: '145652-435', type: '1', responses: 2 },
+      { sample: '145652-435', type: '2', responses: 6 },
+      { sample: '145652-435', type: '3', responses: 20 },
+      { sample: '145652-435', type: '4', responses: 14 },
+      { sample: '145652-435', type: '5', responses: 30 },
+    ],
+  },
+  {
+    key: 3,
+    sample: '866392-926',
+    question: 'q1',
+    numberOfResponses: [
+      { sample: '866392-926', type: '1', responses: 2 },
+      { sample: '866392-926', type: '2', responses: 10 },
+      { sample: '866392-926', type: '3', responses: 5 },
+      { sample: '866392-926', type: '4', responses: 10 },
+      { sample: '866392-926', type: '5', responses: 15 },
+    ],
+  },
+  {
+    key: 5,
+    sample: '59468-622',
+    question: 'q2',
+    numberOfResponses: [
+      { sample: '59468-622', type: '1', responses: 2 },
+      { sample: '59468-622', type: '2', responses: 30 },
+      { sample: '59468-622', type: '3', responses: 7 },
+      { sample: '59468-622', type: '4', responses: 10 },
+      { sample: '59468-622', type: '5', responses: 10 },
+    ],
+  },
+  {
+    key: 6,
+    sample: '59468-622',
+    question: 'q3',
+    numberOfResponses: [
+      { sample: '59468-622', type: '1', responses: 20 },
+      { sample: '59468-622', type: '2', responses: 3 },
+      { sample: '59468-622', type: '3', responses: 7 },
+      { sample: '59468-622', type: '4', responses: 10 },
+      { sample: '59468-622', type: '5', responses: 10 },
+    ],
+  },
+  {
+    key: 7,
+    sample: '59468-622',
+    question: 'q4',
+    numberOfResponses: [
+      { sample: '59468-622', type: '1', responses: 2 },
+      { sample: '59468-622', type: '2', responses: 3 },
+      { sample: '59468-622', type: '3', responses: 7 },
+      { sample: '59468-622', type: '4', responses: 10 },
+      { sample: '59468-622', type: '5', responses: 1 },
+    ],
+  },
+  {
+    key: 8,
+    sample: '59468-622',
+    question: 'q5',
+    numberOfResponses: [
+      { sample: '59468-622', type: '1', responses: 2 },
+      { sample: '59468-622', type: '2', responses: 3 },
+      { sample: '59468-622', type: '3', responses: 7 },
+      { sample: '59468-622', type: '4', responses: 1 },
+      { sample: '59468-622', type: '5', responses: 10 },
+    ],
+  },
+]
+
+let filteredResults
+
+let data
+
+let samples = []
+
 const rowSelection = {
   onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
-    console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows)
+    samples = []
+    selectedRows.forEach(function (row) {
+      samples.push(row.sample)
+    })
+    console.log(`samples: ${samples}`, 'selectedRows: ', selectedRows)
+    renderChart()
   },
   getCheckboxProps: (record: DataType) => ({
     disabled: record.sample === 'Disabled Sample', // Column configuration not to be checked
@@ -66,7 +179,24 @@ const rowSelection = {
   }),
 }
 
-const Demo = () => {
+function renderChart() {
+  data = []
+  let app = document.getElementById('Charts')
+  ReactDOM.unmountComponentAtNode(app)
+  if (filteredResults != null) {
+    filteredResults.forEach(function (response) {
+      if (samples.includes(response['sample'])) {
+        response['numberOfResponses'].forEach(function (input) {
+          data.push(input)
+        })
+      }
+    })
+    console.log(data)
+    ReactDOM.render(<DemoColumn />, app)
+  }
+}
+
+const SampleTable = () => {
   const [selectionType, setSelectionType] = useState<'checkbox'>('checkbox')
 
   return (
@@ -77,14 +207,56 @@ const Demo = () => {
           ...rowSelection,
         }}
         columns={columns}
-        dataSource={data}
+        dataSource={sampleInfo}
       />
     </div>
   )
 }
 
+const DemoColumn = () => {
+  const config = {
+    data,
+    isGroup: true,
+    xField: 'type',
+    yField: 'responses',
+    seriesField: 'sample',
+
+    label: {
+      position: 'middle',
+      layout: [
+        {
+          type: 'interval-adjust-position',
+        },
+        {
+          type: 'interval-hide-overlap',
+        },
+        {
+          type: 'adjust-color',
+        },
+      ],
+    },
+  }
+  return <Column {...config} />
+}
+
 function onChange(value) {
   console.log(`selected ${value}`)
+  filteredResults = []
+  results.forEach(function (response) {
+    if (response['question'] == value) {
+      filteredResults.push(response)
+    }
+  })
+  filteredResults.forEach(function (response) {
+    console.log(response)
+  })
+
+  if (samples.length > 0) {
+    renderChart()
+  } else {
+    let app = document.getElementById('Charts')
+    ReactDOM.unmountComponentAtNode(app)
+  }
 }
 
 function onSearch(val) {
@@ -105,30 +277,33 @@ export default function Results() {
               <Card>
                 <PageHeader className="p-0" title="Overall Ranking"></PageHeader>
                 <Divider />
-                <Demo />
+                <SampleTable />
               </Card>
             </Col>
             <Col flex="auto">
               <Card>
                 <PageHeader className="p-0" title="Results by Question"></PageHeader>
                 <Divider />
-                <Select
-                  showSearch
-                  style={{ width: '100%' }}
-                  placeholder="Select a question"
-                  optionFilterProp="children"
-                  onChange={onChange}
-                  onSearch={onSearch}
-                  filterOption={(input, option) =>
-                    option.children.toString().toLowerCase().indexOf(input.toLowerCase()) >= 0
-                  }
-                >
-                  <Option value="q1">Question 1: Rate the SWEETNESS of this sample</Option>
-                  <Option value="q2">Question 2: Rate the ACIDITY of this sample</Option>
-                  <Option value="q3">Question 3: Rate the COLOR of this sample</Option>
-                  <Option value="q4">Question 4: Rate the AROMA of this sample</Option>
-                  <Option value="q5">Question 5: How would you rate this sample OVERALL?</Option>
-                </Select>
+                <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+                  <Select
+                    showSearch
+                    style={{ width: '100%' }}
+                    placeholder="Select a question"
+                    optionFilterProp="children"
+                    onChange={onChange}
+                    onSearch={onSearch}
+                    filterOption={(input, option) =>
+                      option.children.toString().toLowerCase().indexOf(input.toLowerCase()) >= 0
+                    }
+                  >
+                    <Option value="q1">Question 1: Rate the SWEETNESS of this sample</Option>
+                    <Option value="q2">Question 2: Rate the ACIDITY of this sample</Option>
+                    <Option value="q3">Question 3: Rate the COLOR of this sample</Option>
+                    <Option value="q4">Question 4: Rate the AROMA of this sample</Option>
+                    <Option value="q5">Question 5: How would you rate this sample OVERALL?</Option>
+                  </Select>
+                  <div id="Charts"></div>
+                </Space>
               </Card>
             </Col>
           </Row>
